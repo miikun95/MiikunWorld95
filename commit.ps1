fe5a7d3749b03e26b840d97b1f7670ed67a091f8
miikun95 <miikun95@miikun95.net>
2026-08-21 17:48:26 +0900

    更新

--- commit.ps1
+++ commit.ps1
@@ -7,11 +7,12 @@ sudo git commit -am "更新"
 Write-Progress -Activity "処理中" -Status "2 / 6" -PercentComplete 34
 sudo git push
 Write-Progress -Activity "処理中" -Status "3 / 6" -PercentComplete 51
-sudo git push github
-Write-Progress -Activity "処理中" -Status "4 / 6" -PercentComplete 68
-sudo git push gitlab
-Write-Progress -Activity "処理中" -Status "5 / 6" -PercentComplete 85
 Set-Location /srv/www/git/html/miikunworld95.git/
 sudo git update-server-info
+Write-Progress -Activity "処理中" -Status "4 / 6" -PercentComplete 68
+Set-Location /srv/www/
+sudo git push github
+Write-Progress -Activity "処理中" -Status "5 / 6" -PercentComplete 85
+sudo git push gitlab
 Write-Progress -Activity "処理中" -Status "6 / 6" -PercentComplete 100
 Write-Progress -Activity "処理中" -Complete
