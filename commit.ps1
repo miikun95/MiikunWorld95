23433c04f02928b770e4b834e40491a9c86fd77f
miikun95 <miikun95@miikun95.net>
2026-08-21 17:43:34 +0900

    更新

--- commit.ps1
+++ commit.ps1
@@ -1,17 +1,17 @@
 #!/usr/bin/pwsh
 Set-Location /srv/www/
-Write-Progress -Activity "処理中" -Status "0 / 5" -PercentComplete 0
+Write-Progress -Activity "処理中" -Status "0 / 6" -PercentComplete 0
 sudo git add -A
-Write-Progress -Activity "処理中" -Status "1 / 5" -PercentComplete 20
+Write-Progress -Activity "処理中" -Status "1 / 6" -PercentComplete 17
 sudo git commit -am "更新"
-Write-Progress -Activity "処理中" -Status "2 / 5" -PercentComplete 40
+Write-Progress -Activity "処理中" -Status "2 / 6" -PercentComplete 34
 sudo git push
+Write-Progress -Activity "処理中" -Status "3 / 6" -PercentComplete 51
+sudo git push github
+Write-Progress -Activity "処理中" -Status "4 / 6" -PercentComplete 68
+sudo git push gitlab
+Write-Progress -Activity "処理中" -Status "5 / 6" -PercentComplete 85
 Set-Location /srv/www/git/html/miikunworld95.git/
 sudo git update-server-info
-Set-Location /srv/www/
-Write-Progress -Activity "処理中" -Status "3 / 5" -PercentComplete 60
-sudo git push github
-Write-Progress -Activity "処理中" -Status "4 / 5" -PercentComplete 80
-sudo git push gitlab
-Write-Progress -Activity "処理中" -Status "5 / 5" -PercentComplete 100
+Write-Progress -Activity "処理中" -Status "6 / 6" -PercentComplete 100
 Write-Progress -Activity "処理中" -Complete
